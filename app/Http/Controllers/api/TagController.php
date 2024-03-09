bff42d19a8d4c10db1db591ddd88b6f3878415bc
Gustavo Henrique <gustavohgustavo@hotmail.com>
2024-03-08 22:10:53 -0300

    Style: Correcting indentation.

--- app/Http/Controllers/api/TagController.php
+++ app/Http/Controllers/api/TagController.php
@@ -32,12 +32,13 @@ class TagController extends Controller
      *  )
      * )
      */
-    public function getAllTags(){
-        $tags=Tag::all();
-        return response()->json($tags,200);
+    public function getAllTags()
+    {
+        $tags = Tag::all();
+        return response()->json($tags, 200);
     }
 
-     /**
+    /**
      * @OA\POST(
      *  tags={"Tag"},
      *  summary="Save a new Tag",
@@ -77,16 +78,17 @@ class TagController extends Controller
      *  )
      * )
      */
-    public function store(Request $request){
+    public function store(Request $request)
+    {
         try {
-            $data=$request->all();
-            $data["created_at"]=now();
-            $tag=Tag::create($data);
+            $data = $request->all();
+            $data["created_at"] = now();
+            $tag = Tag::create($data);
 
-            return response()->json($tag,201);
+            return response()->json($tag, 201);
         } catch (\InvalidArgumentException $e) {
             return response()->json(["error" => $e->getMessage()], 422);
-        } catch (\Illuminate\Database\QueryException  $e){
+        } catch (\Illuminate\Database\QueryException  $e) {
             return response()->json(["error" => "Tag já cadastrada"], 409);
         }
     }
@@ -120,12 +122,13 @@ class TagController extends Controller
      *  )
      * )
      */
-    public function delete($id){
-        $tag=Tag::find($id);
-        if($tag==null){
+    public function delete($id)
+    {
+        $tag = Tag::find($id);
+        if ($tag == null) {
             return response()->json(["error" => "Tag não encontrada."], 404);
         }
         $tag->delete();
-        return response()->json("",204);
+        return response()->json("", 204);
     }
 }
